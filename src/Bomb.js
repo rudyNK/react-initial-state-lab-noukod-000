f250bf4f79b2827a396ca50abff0b3dbcd9e5f47
Rudy Ravelin <rudy@noukod.com>
2019-04-03 18:42:31 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,23 @@
 // your Bomb code here!
+
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render() {
+    const seconds = this.state.secondsLeft;
+    let msg = seconds + ' seconds left before I go boom!';
+    if (seconds === 0) {
+      msg = 'Boom!';
+    }
+    return (
+      msg
+    )
+  }
+}
